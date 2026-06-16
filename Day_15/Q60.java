4b6a8ef6e32039c24c5e88e69a02a3116ce8beed
Dev01122 <singhdevansh754@gmail.com>
2026-06-16 22:12:27 +0530

    Added Day 15 Assignment

---
+++ Day_15/Q60.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Q60 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of element = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index++] = arr[i];
+            }
+        }
+
+        while (index < n) {
+            arr[index++] = 0;
+        }
+
+        System.out.print("Array after moving zeroes: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}

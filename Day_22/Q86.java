8b3563be8a55a71feaa427e40858eb50032e8a57
Dev01122 <singhdevansh754@gmail.com>
2026-06-30 06:14:48 +0530

    Add Day 22 Assignment

---
+++ Day_22/Q86.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q86 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine().trim();
+
+        if (sentence.isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = sentence.split("\\s+");
+            System.out.println("Number of words: " + words.length);
+        }
+    }
+}
